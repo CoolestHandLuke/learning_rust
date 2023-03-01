30f1be21e39b5ef2194fb3819e162aec9b925897
Luke Shea <shealuke@yahoo.com>
2023-03-01 12:47:14 -0600

    Initial commit for the variables learning tutorial

---
+++ variables/target/debug/deps/variables-957d77e6fcc2471d.d
@@ -0,0 +1,5 @@
+/home/luke/projects/learning_rust/variables/target/debug/deps/variables-957d77e6fcc2471d: src/main.rs
+
+/home/luke/projects/learning_rust/variables/target/debug/deps/variables-957d77e6fcc2471d.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/luke/projects/learning_rust/variables/target/debug/variables: /home/luke/projects/learning_rust/variables/src/main.rs
