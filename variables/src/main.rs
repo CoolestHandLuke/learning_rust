349c2d2885211b4f024e41d74b69acc3f7e6e282
Luke Shea <shealuke@yahoo.com>
2023-03-02 08:09:08 -0600

    Created a new cargo build to learn variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,15 @@
+use std::io;
+
 fn main() {
-    let x = 5;
+    let a = [1, 2, 3, 4, 5];
+    println!("Please enter an array index: ");
+    let mut index = String::new();
 
-    let x = x + 1;
+    io::stdin().read_line(&mut index).expect("Failed to read line!");
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    let index: usize  = index.trim().parse().expect("Index entered was not a number.");
+    
+    let element = a[index];
 
-    println!("The value of x is: {x}");
+    println!("The value of the element at index {index} is: {element}");
 }
